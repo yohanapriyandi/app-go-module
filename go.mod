853110f7b7e4a3f6870643d5aa1c0149fed28e49
Yohan Apriyandi <cepoangkuningan@gmail.com>
2025-05-29 10:48:31 +0700

    using dependency with go_module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/yohanapriyandi/app-go-module
+
+go 1.24.0
+
+require github.com/yohanapriyandi/first-go-module v0.0.1 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	go_module "github.com/yohanapriyandi/first-go-module"
+)
+
+func main() {
+	fmt.Println(go_module.SayHello())
+}
